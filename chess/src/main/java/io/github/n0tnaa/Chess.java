714646dd6801b2853627bd44306accbbad65ee65
Anton <anton.rychwalski.etu@univ-lille.fr>
2025-02-01 01:19:41 +0100

    maven init

---
+++ chess/src/main/java/io/github/n0tnaa/Chess.java
@@ -0,0 +1,13 @@
+package io.github.n0tnaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class Chess 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Chess !" );
+    }
+}
